76401dffa51ed3e238dd2856eff8864fda1f2f2c
A R MAMUN <123754555+BD-CYBER@users.noreply.github.com>
2023-05-11 11:45:37 +0600

    Add files via upload

---
+++ bEST.py
@@ -0,0 +1,10 @@
+import os, platform, time, sys
+
+try:
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
+
+print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;97mChecking For Update...')
+os.system('git pull --quiet 2>/dev/null')
+ print('\033[1;91m[\033[1;97m✓\033[1;91m] \033[1;97mFile Cloning Tools Update')
+ import bEST
